6ab69ddbc9d8207a419624243a7e4c7abf5c4a4b
João_Victor <69221496+cptarqueiro@users.noreply.github.com>
2020-09-18 00:13:57 -0300

    Adicionado adaptador lógico.
    Adicionado adaptador lógico de matemática para efetuar cálculos.

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,18 @@ from chatterbot.trainers import ListTrainer
 
 # Criar uma nova instancia ChatBot
 # usar o read_only=True) quando o bot já estiver treinado, assim ele para de treinar e carrega mais rápido.
-chatbot = ChatBot('Ordis')
+chatbot = ChatBot('Ordis',
+                  storage_adapter='chatterbot.storage.SQLStorageAdapter',
+                  logic_adapters=[
+                      'chatterbot.logic.MathematicalEvaluation',
+                      'chatterbot.logic.BestMatch',
+                      {
+                          'import_path': 'chatterbot.logic.BestMatch',
+                          'default_response': 'Sinto muito, mas não entendo. Ainda estou aprendendo.',
+                          'maximum_similarity_threshold': 0.90
+                      }
+                  ],
+                  database_uri='sqlite:///database.sqlite3')
 
 # Treinamento informativo sobre o corona
 training_data_informativo = open('training_data/corona.txt', encoding='utf-8').read().splitlines()
